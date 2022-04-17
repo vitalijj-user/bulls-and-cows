d59a16b7dcc16ec78b6eb61bafa33bc7b5e2f87b
Vitaliy Sazonov <1111vitalijj@gmail.com>
2022-04-17 18:28:51 +0300

    Бики і Корови 1.1

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from mastermind_engine import report_count, number_correct, bull_cow_count
-
-
-
+import os
+from mastermind_engine import report_count, \
+    number_correct, bull_cow_count, number_generator
+from termcolor import cprint, colored
 
 while True:
-    number = input("Введіть чотирьохзначне число")
-    if number_correct(number):
-        if report_count():
-            print("Ура, ви вгадали це число за {} спроб".format(report_count()))
-            break
+    response = input(colored("Бажаєте ще зіграти? ", color='yellow'))
+    if response == "q" or response == "й":
+        break
+    os.system('CLS')
+    number_generator()
+    while True:
+        number = input(colored("Введіть чотирьохзначне число: ", color='blue'))
+        if response == "q" or response == "q":
+            os.system('EXIT')
+        if number_correct(number):
+            bull = bull_cow_count()["bull"]
+            cow = bull_cow_count()["cow"]
+            report = report_count()
+            if report:
+                print("Ура, ви вгадали це число за {} спроб".format(report))
+                break
+            else:
+                print("Биків: ", bull)
+                print("Корів: ", cow)
         else:
-            print("Биків", bull_cow_count())
-            print("Корыв", bull_cow_count())
-    else:
-        print("Ви ввели не коректне число")
-
-
+            print("Ви ввели не коректне число")
--- mastermind_engine.py
+++ mastermind_engine.py
@@ -1,17 +1,73 @@
+from random import randint
 
-_number = 0
+_random_number = []
+_enter_number = "0"
+_enter_inputs = 0
+_count = {"cow": 0, "bull": 0}
+
 
 def number_generator():
-    pass
+    """
+    Компютер загадує число
+    """
+    global _random_number
+    global _enter_inputs
+    _enter_inputs = 0
+    i = []
+    while len(i) < 4:
+        num = randint(1, 9)
+        if num not in i:
+            i.append(num)
+    _random_number = i
+
 
 def number_correct(number):
-    """ Перевірка правильності вводу """
-    return number
+    """"
+    Перевірка правильності вводу
+    """
+    if len(number) != 4:
+        return False
+    for i in number:
+        if list(number).count(i) > 1:
+            return False
+        elif int(i) == 0:
+            return False
+    global _enter_number
+    _enter_number = number
+    return _enter_number
+
+
 
 def report_count():
-    """ Якщо вірний результат вертає кількість спроб int"""
-    pass
+    """
+    Кількість спроб відгадати число
+    """
+    global _enter_inputs
+    _enter_inputs += 1
+    if _count["bull"] == 4:
+        return _enter_inputs
+
 
 def bull_cow_count():
-    """ Кількість биків, кількість корів dict"""
-    pass
+    """
+    Кількість биків, кількість корів dict
+    """
+    global _count
+    _count = {"cow": 0, "bull": 0}
+    inc = 0
+    for i in _enter_number:
+        if int(i) in _random_number:
+            _count["cow"] += 1
+        if int(i) == _random_number[inc]:
+            _count["bull"] += 1
+        inc += 1
+    return _count
+
+
+if __name__ == "__main__":
+    number_correct("1234")
+    print(_enter_number)
+
+
+
+
